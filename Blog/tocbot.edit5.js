bf51dfd5911fc8f5897aaf112903b46db012ba87
huanrcz <76637905+huanrcz@users.noreply.github.com>
2021-07-09 02:46:42 +0800

    Update and rename tocbot.edit4.js to tocbot.edit5.js

--- Blog/tocbot.edit5.js
+++ Blog/tocbot.edit5.js
@@ -234,7 +234,7 @@
             var o, l = t;
             if (m && null !== document.querySelector(e.tocSelector) && l.length > 0) {
                 d.call(l, function(t, i) {
-                    if (t.offsetTop > n + 560) {
+                    if (t.offsetTop > n + 510) {
                         return o = l[0 === i ? i : i - 1],
                         !0
                     }

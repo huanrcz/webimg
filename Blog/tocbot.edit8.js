ea973371996a0414ce79acb69b14938c3bafca43
huanrcz <76637905+huanrcz@users.noreply.github.com>
2021-07-09 02:48:39 +0800

    Update and rename tocbot.edit7.js to tocbot.edit8.js

--- Blog/tocbot.edit8.js
+++ Blog/tocbot.edit8.js
@@ -234,7 +234,7 @@
             var o, l = t;
             if (m && null !== document.querySelector(e.tocSelector) && l.length > 0) {
                 d.call(l, function(t, i) {
-                    if (t.offsetTop > n + 410) {
+                    if (t.offsetTop > n + 360) {
                         return o = l[0 === i ? i : i - 1],
                         !0
                     }
